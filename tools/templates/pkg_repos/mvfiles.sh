c1173c11d0d4e125b35f4d8718ba2af990de9d14
Charlie Root <root@freebsd12p1.swag.local>
2020-07-06 11:11:22 -0700

    made changes per the building-pfsense-iso-from-source HOW-TO

---
+++ tools/templates/pkg_repos/mvfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mv pfSense-repo-244.abi mySense-repo-244.abi
+mv pfSense-repo-244.altabi mySense-repo-244.altabi
+mv pfSense-repo-244.conf mySense-repo-244.conf
+mv pfSense-repo-244.descr mySense-repo-244.descr
+mv pfSense-repo-devel.abi mySense-repo-devel.abi
+mv pfSense-repo-devel.altabi mySense-repo-devel.altabi
+mv pfSense-repo-devel.conf mySense-repo-devel.conf
+mv pfSense-repo-devel.descr mySense-repo-devel.descr
+mv pfSense-repo.abi mySense-repo.abi
+mv pfSense-repo.altabi mySense-repo.altabi
+mv pfSense-repo.conf mySense-repo.conf
+mv pfSense-repo.descr mySense-repo.descr
